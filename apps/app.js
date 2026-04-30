eaa275ffd0e4111c5f3c0fa2748f5c577713c417
mahalakshmi191 <107101381+mahalakshmi191@users.noreply.github.com>
2026-04-30 22:33:15 +0530

    Update and rename apps/myapp/node.js to apps/app.js

--- apps/app.js
+++ apps/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req,res)=> res.send("GitOps Live App"));
+app.get("/", (req,res)=> res.send("GitOps Working"));
 
 app.listen(3000);
